a4ec2e35099d8ac9b04c176612f65a29d416fe49
avalanche <avalanche@Fractal>
2014-04-30 08:19:13 -0700

    small fix

--- DrinkUtility/src/onedrinkaway/DBUtility.java
+++ DrinkUtility/src/onedrinkaway/DBUtility.java
@@ -24,7 +24,7 @@ public class DBUtility {
 
     public static void main(String[] args) {
         //List<String> drinks = getDrinkNames();
-        scrapeGoodCocktails(1501, 2000, 4);
+        scrapeGoodCocktails(1, 100, 1);
     }
     
     /**
