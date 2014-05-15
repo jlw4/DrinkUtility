077fb832b7da8068c0b6c52e2f2393771ef92057
John L Wilson <johnleewilson4@gmail.com>
2014-05-15 14:29:26 -0700

    asdf

--- Drink Utility/src/TestDrinkData.java
+++ Drink Utility/src/TestDrinkData.java
@@ -1,34 +1,13 @@
 import java.io.File;
 import java.io.PrintWriter;
 import java.util.Scanner;
-import java.util.Set;
-
-import com.onedrinkaway.common.Drink;
-import com.onedrinkaway.common.DrinkInfo;
-import com.onedrinkaway.db.DrinkData;
-import com.onedrinkaway.db.DrinkDb;
 
 
 public class TestDrinkData {
     
-    private static DrinkData dd;
 
     public static void main(String[] args) {
         fixRecipesLarge();
-    }
-    
-    private static void printAllDrinkInfo() {
-        dd = DrinkData.getDrinkData();
-        Set<Drink> drinks = dd.getAllDrinks();
-        for (Drink d : drinks) {
-            DrinkInfo di = dd.getDrinkInfo(d);
-            System.out.println(di.description);
-            System.out.println(di.drinkId);
-            System.out.println(di.garnish);
-            System.out.println(di.instructions);
-            System.out.println(di.source);
-            System.out.println();
-        }
     }
     
     private static void fixRecipesLarge() {
