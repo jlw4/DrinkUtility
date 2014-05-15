daa8009d66e1ddd9697dda5471a115ca0ca88dbd
John L Wilson <johnleewilson4@gmail.com>
2014-05-15 11:19:58 -0700

    changes!

---
+++ Drink Utility/src/DrinkInfoFinder.java
@@ -0,0 +1,13 @@
+
+public class DrinkInfoFinder {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+    
+    private static void generateIngredientsList(String name) {
+        
+    }
+
+}
--- Drink Utility/src/TestDrinkData.java
+++ Drink Utility/src/TestDrinkData.java
@@ -1,3 +1,10 @@
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.Set;
+
+import com.onedrinkaway.common.Drink;
+import com.onedrinkaway.common.DrinkInfo;
 import com.onedrinkaway.db.DrinkData;
 import com.onedrinkaway.db.DrinkDb;
 
@@ -7,10 +14,40 @@ public class TestDrinkData {
     private static DrinkData dd;
 
     public static void main(String[] args) {
-        String[] names = DrinkDb.getDrinkNames();
-        for (String s : names)
-            System.out.println(s);
-
+        DrinkDb.saveDrinkData();
+    }
+    
+    private static void printAllDrinkInfo() {
+        dd = new DrinkData();
+        Set<Drink> drinks = dd.getAllDrinks();
+        for (Drink d : drinks) {
+            DrinkInfo di = dd.getDrinkInfo(d);
+            System.out.println(di.description);
+            System.out.println(di.drinkId);
+            System.out.println(di.garnish);
+            System.out.println(di.instructions);
+            System.out.println(di.source);
+            System.out.println();
+        }
+    }
+    
+    private static void fixIngredientsLarge() {
+        try {
+            String fileName = "IngredientsFixed.txt";
+            File file = new File(fileName);
+            PrintWriter pw = new PrintWriter(file);
+            Scanner sc = new Scanner(new File("IngredientsLarge.txt"));
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                if (line.contains("<div")) {
+                    int i = line.indexOf("<");
+                    line = line.substring(0, i);
+                }
+                pw.println(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
