9d2f8815159a4a1e874e64edaafae1139e351212
John L Wilson <johnleewilson4@gmail.com>
2014-05-29 23:07:23 -0700

    Updated Recipe file, removing duplicate ingredients. Updated serialized DrinkData

--- Drink Utility/src/SaveDrinkData.java
+++ Drink Utility/src/SaveDrinkData.java
@@ -9,6 +9,7 @@ public class SaveDrinkData {
         try {
             DrinkData dd = DrinkData.getDrinkDataDB("test");
             dd.saveDrinkDataDebug(new FileOutputStream("drinkdata.ser"));
+            System.out.println("DrinkData saved");
         } catch (Exception e) {
             e.printStackTrace();
         }
